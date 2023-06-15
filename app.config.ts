4509883f748bebae9ba862f5d487ef34ec3bf72e
Matheus <matheusalmeida016@gmail.com>
2023-06-15 15:52:37 -0400

    feat: initial commit

---
+++ app.config.ts
@@ -0,0 +1,27 @@
+import { version } from 'package.json'
+
+module.exports = {
+  expo: {
+    name: 'track solar',
+    slug: 'track-solar',
+    version: version,
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#14B8A6',
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#14B8A6',
+      },
+    },
+  },
+}
