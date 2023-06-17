252d3b12d2932bd2fc69ef66cce084e8099d0b23
Matheus <matheusalmeida016@gmail.com>
2023-06-17 04:47:54 -0400

    feat: add login screen

--- src/screens/credential/credential.tsx
+++ src/screens/credential/credential.tsx
@@ -1,11 +1,32 @@
-import { Text } from '@/styles'
+import { Logo } from '@/assets/logo'
+import { Button } from '@/components/button'
+import { useTheme } from 'styled-components/native'
+import { useState } from 'react'
 
 import * as S from './style'
 
 export const Credential = () => {
+  const theme = useTheme()
+  const [value, setValue] = useState('')
+
   return (
     <S.Container>
-      <Text>Credential</Text>
+      <Logo />
+
+      <S.FieldWrapper>
+        <S.TextInput
+          placeholder="Sua credencial aqui"
+          cursorColor={theme.colors.white}
+          placeholderTextColor={theme.colors.cyan200}
+          selectionColor={theme.colors.cyan200}
+          inputMode="text"
+          onChangeText={setValue}
+          value={value}
+          onSubmitEditing={(e) => console.log(e.nativeEvent.text)}
+        />
+
+        <Button>Entrar</Button>
+      </S.FieldWrapper>
     </S.Container>
   )
 }
--- src/screens/credential/style.ts
+++ src/screens/credential/style.ts
@@ -2,8 +2,25 @@ import styled from 'styled-components/native'
 
 export const Container = styled.SafeAreaView`
   flex: 1;
-  justify-content: flex-end;
+  justify-content: center;
+  align-items: center;
   gap: 74px;
-  padding: 0 0 54px;
+  padding: 0 16px 54px;
   background-color: ${({ theme }) => theme.colors.cyan300};
+`
+
+export const FieldWrapper = styled.View`
+  align-self: stretch;
+  flex-direction: column;
+  gap: 24px;
+`
+
+export const TextInput = styled.TextInput`
+  height: 49px;
+  background-color: ${({ theme }) => theme.colors.white};
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.cyan300};
+  font-family: ${({ theme }) => theme.fontFamily.DMSansRegular};
+  padding: 0 16px;
+  border-radius: 10px;
 `
